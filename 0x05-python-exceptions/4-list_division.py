5a53769de9e8ac3fa5c1a98d413a0e91f74e29bc
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-25 12:17:30 -0700

    V

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,6 +3,12 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
+            element1 = my_list_1[i] if i < len(my_list_1) else None
+            element2 = my_list_2[i] if i < len(my_list_2) else None
+            if element1 is None or element2 is None:
+                raise IndexError("out of range")
+            result = element1 / element2
+        try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrongtype")
