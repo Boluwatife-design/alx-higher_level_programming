2cecd099f6c62d61040671cb7fedf5a0ca70632e
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-25 05:45:41 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             for item in my_list:
-                print(item, end=" ")
+                print(item, end="")
         except Exception as e:
             print(f"Error: {e}")
             break
