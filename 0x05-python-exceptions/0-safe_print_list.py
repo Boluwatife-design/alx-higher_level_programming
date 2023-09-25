9869a0e5e074a072c655a34cf21eaa9ed2177cb0
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-25 05:41:56 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            for item in my_list:
+                print(item, end=" ")
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+    print("")
