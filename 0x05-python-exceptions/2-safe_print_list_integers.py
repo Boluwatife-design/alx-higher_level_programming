149654e1343f794b5032c07700982b01c70b4a3e
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-25 11:33:17 -0700

    2-safe_print_list_integers.p

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-"""print first element of a list
-"""
+    """print first element of a list
+    """
     print_count = 0
     for i in range(x):
         try:
