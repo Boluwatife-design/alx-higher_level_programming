624d3d053cf4456de7b98ac397417fb60753284b
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-26 09:40:38 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
 
     @property
     def size(self):
-        """set the current size of the square."""
+        """get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
