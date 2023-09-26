f2b637a01cdf9b789413cb90205e04cd96a85104
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-09-26 07:43:58 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size)
+    """initialize a new square.
+    :size(int): size of a new square
+    """
+    self.size = size
