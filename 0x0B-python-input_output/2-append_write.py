1903db98f7d537a0bd434666c8c8b44942dd45b2
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-10-10 07:47:45 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that appends a string"""
+"""Defines a function that appends a file"""
 
 
 def append_write(filename="", text=""):
@@ -7,7 +7,7 @@ def append_write(filename="", text=""):
     
     :filename(str):name of file to append to
     :text(str):str to append to file
-    Returns: number of character added
+    :Returns: number of character added
     """
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
