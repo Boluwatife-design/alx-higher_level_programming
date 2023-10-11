c43ed50dae82968d2ff4b4dae85e8c652b0bb59e
Boluwatife-design <temitopeolowoniyi12@gmail.com>
2023-10-11 14:41:17 -0700

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class that inherit from my list"""
+"""Defines a class that inherit from MyList"""
 
 
 class MyList(list):
-    """class that inherit from my list"""
+    """Implements sorted printing for the built-in list class."""
     
     def print_sorted(self):
         """prints a sorted list"""
